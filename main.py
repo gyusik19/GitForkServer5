185b621fce5c370d8583f1eb8088fdf00741eddb
GyuSik Choi <gyusik19@gmail.com>
2021-04-13 23:19:36 +0900

    Update in leader

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ print("Hello World 1 in team leader server")
 
 print("Hello World 2 in team leader server")
 
+print("Update in leader")
+
 print("Hello World 3 in team member C local server")
 
 print("Hello World 3 in team member B local server")
 
 print("Hello World 3 in team member A local server")
 
-print("Update in leader")
